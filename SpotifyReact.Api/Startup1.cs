80f13370cac29f1f3e757b0705a4ade023fb46c5
John Nolan <johnnolan@nolanscafe.co.uk>
2015-05-25 15:24:18 +0100

    .Net project and basic files added

---
+++ SpotifyReact.Api/Startup1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using SpotifyReact.Api;
+
+[assembly: OwinStartup(typeof(Startup1))]
+
+namespace SpotifyReact.Api
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+
+            app.Run(context =>
+            {
+                if (context.Request.Path.ToString() == "/fail")
+                {
+                    throw new Exception("Random exception");
+                }
+
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
---
+++ SpotifyReact.Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SpotifyReact.Api;
+
+namespace SpotifyReact.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup1>("http://localhost:9000"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
